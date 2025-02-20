355ff0c96ffa436b7e0f2a486ab8ad21f8321249
Shoaibali12 <sind98449@gmail.com>
2025-02-20 10:47:53 -0800

    create signin api route

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { registerUser } from "../controllers/userController.js";
+import { registerUser, authUser } from "../controllers/userController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.post("/register", registerUser);
+router.post("/login", authUser);
 
 export default router;
